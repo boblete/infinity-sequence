59e95ba660302097264f9b37a8f62703cda573b6
Ouroboros4991 <rubendejaegere@hotmail.com>
2018-04-07 16:41:29 +0100

    start merging

--- src/components/app.js
+++ src/components/app.js
@@ -1,15 +1,13 @@
 import React from 'react';
-import Stave from './score/stave';
-import Dropdown from 'react-dropdown';
-import {convertMidiToNote,convertNoteToMidi} from '../utils/helpers';
-import Engine from './engine/engine';
+import Stave from './score/stave'
+import Dropdown from 'react-dropdown'
+import SheetMusic from './sheetRenderer';
+import {convertMidiToNote,convertNoteToMidi} from '../utils/helpers'
+import Engine from './engine/engine'
 /*
-
 The Danish composer Per Nørgård's "infinity sequence",
 invented in an attempt to unify in a perfect way repetition and variation:
 a(2n) = -a(n), a(2n+1) = a(n) + 1, a(0)=0.
-
-
 */
 
 
@@ -19,9 +17,8 @@ const OCTAVES = [1,2,3,4,5,6,7,8];
 class App extends React.Component {
      constructor(props) {
         super();
-        this.engine = new Engine();
+        this.engine = new Engine()
      }
-
     state = {
         staveVisible: false,
         options:NOTES,
@@ -30,6 +27,7 @@ class App extends React.Component {
         defaultOctaves:OCTAVES[3],
         totalNotes:100,
         startNote:0,
+        currentNote:60,
         currentOption:'C',
         noteContainer:[]
     }
@@ -39,19 +37,22 @@ class App extends React.Component {
 import 'react-dropdown/style.css'
 const defaultOption = options[0]
 <Dropdown options={options} onChange={this._onSelect} value={defaultOption} placeholder="Select an option" />
-
     */
 
     componentDidMount () {
-        this.getSeries();
+        this.getSeries()
+        this.engine.play(this.state.noteContainer)
     }
-
-    componentDidUpdate(prevProps, prevState, snapshot){
-        this.engine.play(this.state.noteContainer);
+    componentWillUpdate(nextProps,nextState){
+        console.log(nextState,this.state);
+        if(nextState.currentOption!==this.state.currentOption){
+            console.log('nextState',nextState.currentOption,this.state.currentOption);
+            //this.setState()
+            this.state.currentOption=nextState.currentOption
+            this.getSeries();
+            //this.renderSeries();
+        }
     }
-
-
-
     getSeries(){
         var pn = [0,1]
         var endSeries = this.state.startNote+ this.state.totalNotes;
@@ -63,53 +64,66 @@ const defaultOption = options[0]
             pn[2*i] = pn[2*i -2] - (pn[i] - pn[i-1])
             pn[2*i +1] = pn[2*i -1] + (pn[i] - pn[i-1])
         */
-        var startNote=convertNoteToMidi(this.state.currentOption + OCTAVES[3]);
-        this.setState({startNote, startNote});
-        //console.log(this.state.startNote);
+        console.log(this.state.currentOption , OCTAVES[3])
+        var currentNote=convertNoteToMidi(this.state.currentOption + OCTAVES[3]);
+        this.setState({currentNote, currentNote});
+        console.log(this.state.startNote);
         var noteContainer = [];
-        for(var j = 0 ; j<=endSeries;j++  ){
-            noteContainer.push(convertMidiToNote(startNote - pn[j]));
+        for(var j = this.state.startNote ; j<=endSeries;j++  ){
+            noteContainer.push(convertMidiToNote(currentNote - pn[j]));
         }
-        //console.log(noteContainer);
+        console.log(noteContainer);
         this.setState({noteContainer , noteContainer});
-        this.renderSeries();
+      //  this.renderSeries();
     }
+
     renderSeries() {
         let s = ""
         let noteContainer = this.state.noteContainer;
+        console.log(this.state.noteContainer);
         for(var k = 0 ; k <noteContainer.length; k++){
             s += noteContainer[k]+ ", ";
         }
         return s;
-
     }
     _onSelect(e){
         console.log(e,this);
         let currentOption = e.value;
         this.setState({currentOption , currentOption}, this.getSeries());
     }
-
-    render(){
-        let { staveVisible,options ,defaultOption} = this.state;
+    render() {
+        let { staveVisible,options ,defaultOption,currentOption} = this.state;
         let { actions, visibleInstrument, volume, inputMode, registerOfflineHook, registerOnlineHook } = this.props;
         let _onSelect = this._onSelect
+
+        console.log('render');
         return (
             <div className='is-container'>
             <h1>Infinity series</h1>
             <div className='span1'>
-            <Dropdown options={options} onChange={(e) =>this._onSelect(e)} value={defaultOption} placeholder="Select an option" />
+            <Dropdown options={options} onChange={(e) =>this._onSelect(e)} value={currentOption} placeholder="Select an option" />
             </div>
              <div className='series'>
             { this.renderSeries()
             }
             </div>
+              <div className="vexFlow">
+                    <SheetMusic notes={this.state.noteContainer}/>
+                </div>
 
-            { staveVisible && (
-                    <Stave
-                        {...this.props}
-                        {...this.state}
-                    />
-                ) }
+
+                {/* { staveVisible && (
+                        <Stave
+
+                            {...this.props}
+                            {...this.state}
+                        />
+                    ) }
+                <Engine
+                    {...this.props}
+                    {...this.state}
+                /> */}
+
             </div>
         )
     }
