4905fb63496d9521fff11b9dbe53027c5b3a6824
Bob Smith <Boblete@Alans-MBP.guest.visitor>
2018-04-07 19:16:23 +0100

    Intervals fixed

--- src/components/app.js
+++ src/components/app.js
@@ -15,18 +15,21 @@ a(2n) = -a(n), a(2n+1) = a(n) + 1, a(0)=0.
 
 const NOTES = ['C', 'C#', 'D', 'D#', 'E', 'F', 'F#', 'G', 'G#', 'A', 'A#', 'B'];
 const OCTAVES = [1,2,3,4,5,6,7,8];
+const INTERVAL = [-12,-11,-10,-9,-8,-7,-6,-5,-4,-3,-2,-1,0,1,2,3,4,5,6,7,8,9,10,11,12];
+
 class App extends React.Component {
      constructor(props) {
         super();
         this.engine = new Engine();
      }
+
     state = {
         staveVisible: false,
         options:NOTES,
         defaultOption:NOTES[0],
         octaveOptions:OCTAVES,
         currentOctaveOption:OCTAVES[3],
-        intervalOptions:OCTAVES,
+        intervalOptions:INTERVAL,
         currentIntervalOption:1,
         octaves:OCTAVES,
         defaultOctaves:OCTAVES[3],
