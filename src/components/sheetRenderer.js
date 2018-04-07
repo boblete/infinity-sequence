f3b3f831486d430f04ac92705a88881850919155
Gideon <gideonb234@gmail.com>
2018-04-07 17:49:34 +0100

    change width

--- src/components/sheetRenderer.js
+++ src/components/sheetRenderer.js
@@ -13,7 +13,7 @@ class SheetMusic extends React.Component {
         return this.props.notes;
     }
 
-    initNotesAsVexflowObjects(notes) {
+    initNotesAsVexflowObject(notes) {
         let vexFlowNotes = [];
 
         notes.forEach((elem, index) => {
@@ -24,30 +24,34 @@ class SheetMusic extends React.Component {
                 duration: "w",
             }));
         });
+
         return vexFlowNotes;
     }
 
+    createStave() {
+        return new Stave(0,0,1200);
+    }
+
     renderVexFlow() {
-        const chord = this.initNotesAsVexflowObjects(this.props.notes);
+        const chord = this.initNotesAsVexflowObject(this.props.notes);
 
         const svgContainer = document.createElement('div');
         const renderer = new Renderer(svgContainer, Renderer.Backends.SVG);
         const ctx = renderer.getContext();
-        const stave = new Stave(0, 0, 100);  // x, y, width
+        const stave = new Stave(0, 0, 1200);  // x, y, width
         stave.addClef("treble").setContext(ctx).draw();
         const bb = Formatter.FormatAndDraw(ctx, stave, chord);
-
+        
         const svg = svgContainer.childNodes[0];
         const padding = 10;
         const half = padding / 2;
-        svg.style.top = -bb.y + half + Math.max(0, (100 - bb.h) * 2/3) + "px";
-        svg.style.height = Math.max(100, bb.h);
+        svg.style.height = Math.max(1200, bb.h);
         svg.style.left = "0px";
-        svg.style.width = 100 + "px";
+        svg.style.width = 1200 + "px";
         svg.style.position = "absolute";
         svg.style.overflow = "visible";
         svgContainer.style.height = Math.max(100, bb.h + padding) + "px";
-        svgContainer.style.width = 100 + "px";
+        svgContainer.style.width = 1200 + "px";
         svgContainer.style.position = "relative";
         svgContainer.style.display = "inlineBlock";
 
