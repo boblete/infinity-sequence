a4069d58cad92d74eb6547cdfdf285bce94b00a9
Bob Smith <Boblete@Alans-MacBook-Pro.local>
2018-04-08 09:08:57 +0100

    Bobs synth stuff

--- src/components/engine/engine.js
+++ src/components/engine/engine.js
@@ -3,11 +3,34 @@ import Tone from 'tone';
 
 export default class Engine{
     constructor(){
-        this.synth = new Tone.Synth();
+        this.synth = new Tone.Synth({
+                            oscillator : {
+                            type : 'fmsquare',
+                            modulationType : 'sawtooth',
+                            modulationIndex : 3,
+                            harmonicity: 3.4
+                          },
+                          envelope : {
+                            attack : 0.001,
+                            decay : 0.1,
+                            sustain: 0.1,
+                            release: 1
+                          }
+                      });
         this.synth.toMaster();
         this.seq;
 
-        this.synth2 = new Tone.Synth();
+        this.synth2 = new Tone.Synth({
+                            oscillator : {
+                            type : 'triangle8'
+                          },
+                          envelope : {
+                            attack : 2,
+                            decay : 1,
+                            sustain: 0.4,
+                            release: 4
+                        }
+                    });
         this.synth2.toMaster();
         this.seq2;
     }
